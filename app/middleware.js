45a62a6072b03c4bac4336e6cfda5089cd280693
charlesoller <charlesrello@gmail.com>
2023-11-04 11:35:50 -0400

    Still CORS

--- app/middleware.js
+++ app/middleware.js
@@ -2,21 +2,12 @@ import { createServerClient } from '@supabase/ssr'
 import { NextResponse } from 'next/server'
 import { corsHeaders} from '../_shared/cors.ts'
 
-export function middleware() {
-  // retrieve the current response
-  const res = NextResponse.next()
-
-  // add the CORS headers to the response
-  res.headers.append('Access-Control-Allow-Credentials', "true")
-  res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
-  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-  res.headers.append(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-
-  return res
-}
+export async function middleware(request) {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers, ...corsHeaders
+    }
+  })
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
